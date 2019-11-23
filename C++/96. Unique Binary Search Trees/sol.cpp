c585c88a3ea9e69357f9548d05b74238ed41cf4b
Hael Chan <f.procumbens@gmail.com>
2019-11-22 23:33:07 -0800

    slight modification of solution to 96

--- C++/96. Unique Binary Search Trees/sol.cpp
+++ C++/96. Unique Binary Search Trees/sol.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int numTrees(int n) {
-        if (!n) 
-            return 1;
-        
         vector<int> dp(n + 1, 0);
         dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            int available = 0;
+        for (int i = 1; i <= n; i++) {
+            int comb = 0;
             for (int j = 1; j <= i; j++) {
-                available += dp[j - 1] * dp[i - j];
+                comb += dp[j - 1] * dp[i - j];
             }
-            dp[i] = available;
+            dp[i] = comb;
         }
         return dp[n];
     }
